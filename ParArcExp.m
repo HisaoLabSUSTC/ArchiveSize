35c61b0cfc6dcc2b453b8e651bf740a5abf28dd5
Tianye Shu <47651611+SliverySky@users.noreply.github.com>
2022-09-01 14:49:32 +0800

    Update ParArcExp.m

--- ParArcExp.m
+++ ParArcExp.m
@@ -3,36 +3,31 @@ problems =["DTLZ1","DTLZ2","DTLZ3","DTLZ4","MinusDTLZ1", "MinusDTLZ2", "MinusDTL
 objs = [3,5,8];%[3,5,8]; % all should be preserve
 PF =  containers.Map;
 Gens = [400,600,750; 250,350,500; 1000,1000,1000; 600,1000,1250;400,600,750; 250,350,500; 1000,1000,1000; 600,1000,1250];
-arcs = [1,1; 2,1; 2,2; 2,5; 2,10;2,20; 3,1];%[1,1; 2,1; 2,2; 2,5; 2,10;2,20; 3,1];
-folder = "/home/vmuser/Dataset/ArchiveSize/";
+arcs = [1,1];%[1,1;2,1;2,10];%[1,1; 2,1; 2,2; 2,5; 2,10;2,20; 3,1];%[1,1; 2,1; 2,2; 2,5; 2,10;2,20; 3,1];
+
+%folder = "/home/vmuser/Dataset/Update/";
+folder = "/home/vmuser/server/dataset/ArchiveSize_Revision";
+loadfolder = "/home/vmuser/Dataset/ArchiveSize";
 savefolder = sprintf("%s/FinalArchive/",folder);
+removalMethod="List";
+truncationMethod="DSS";
 %Get the PF for each experiment
 for b=1:length(problems)
     for c=1:length(objs)
-       % sprintf('PF(\"%s_%d\")=%s(''M'',%d);',problems(b), objs(c), problems(b),objs(c))
         eval(sprintf('PF(\"%s_%d\")=%s(''M'',%d).optimum;',problems(b), objs(c), problems(b),objs(c)));
     end
 end
 
 
-
+parfor i = 1:63
+    x=1+1;
+end
 for b=1:length(problems)
     for c=1:length(objs)
         for e=1:size(arcs,1)
 %             if (ismember(num2str(objs(c))+problems(b),...
-%                 ["3MinusDTLZ1", "5DTLZ3", "8MinusDTLZ2"]))
-%                 continue
-%             end
-            if ~(ismember(num2str(objs(c))+problems(b),...
-                ["5MinusDTLZ3", "8MinusDTLZ3", "3MinusDTLZ4", "5MinusDTLZ4","8MinusDTLZ4"]))
-                continue
-            end
-            if ismember(num2str(objs(c))+problems(b),...
-                ["5MinusDTLZ3"]) && e<=4
-                continue
-            end
-%             if ismember(num2str(objs(c))+problems(b),["5DTLZ3"]) && e==1
-%                 continue
+%                 ["3MinusDTLZ1", "5DTLZ3"])) %,"5DTLZ3"
+%                 continue;
 %             end
             if objs(c)==3
                 pop = 91;
@@ -41,11 +36,7 @@ for b=1:length(problems)
             else
                 pop = 156;
             end
-            for arcsiz = [1,2,5,10,20,50,100,200,500,1000,2000]*pop
-%                 if ~(ismember(num2str(objs(c))+problems(b),...
-%                  ["8MinusDTLZ2"]) && arcsiz==500*pop && e==1)
-%                     continue
-%                 end
+            for arcsiz = [1,1,2,5,10,20,50,100,200,500,1000,2000]*pop
                 clear task
                 n=0;
                 for a=1:length(algos)
@@ -72,9 +63,11 @@ for b=1:length(problems)
                     pop = task(index).pop;
                     id = task(index).id;
                     name = sprintf("%s_%s_N%d_M%d_%d",algo, problem, pop, obj, id);
-                    data = load(sprintf("%s/Data/%s",folder,name));
+                    data = load(sprintf("%s/Data/%s",loadfolder,name));
                     para = task(index).para;
                     arcst = ArchiveStrategy(para{1}, para{2}, para{3}, para{4});
+                    arcst.removalMethod=removalMethod;
+                    arcst.truncationMethod=truncationMethod;
                     arcst.setName(sprintf("%s_%s_M%d",algo, problem, obj), id);
                     arcst.name
                     arcst.update(data.rec);
